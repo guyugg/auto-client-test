cca4be194faf22ea6969f75a4ca937f5c2d91635
roger0574 <roger0574@cchntek.com>
2026-07-23 16:02:23 +0800

    [修改] isProduction判斷

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,50 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: './'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js',
+      publicPath: isProduction ? './' : '/'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
         }
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx']
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ],
-  devServer: {
-    historyApiFallback: true,
-    port: 3000,
-    hot: true,
-    proxy: [
-      {
-        context: ['/api'],
-        target: 'http://localhost:5001',
-        changeOrigin: true
-      }
-    ]
-  }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.jsx']
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      })
+    ],
+    devServer: {
+      historyApiFallback: true,
+      port: 3000,
+      hot: true,
+      proxy: [
+        {
+          context: ['/api'],
+          target: 'http://localhost:5001',
+          changeOrigin: true
+        }
+      ]
+    }
+  };
 };
